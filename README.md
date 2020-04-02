1dea816a260851b40ae2e06c13667804a4d5bae3
ypvinson <ypvinson@gmail.com>
2020-04-01 20:12:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-file-upload-preview-bzg249
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-file-upload-preview-bzg249)
---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 4';
+  url = '';
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      }
+    }
+  }
+}
